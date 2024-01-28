6962d11ca828a3f95e0bf801da076ee9df800ea7
lucka-me <i@lucka.moe>
2024-01-28 18:07:22 +0800

    Improve coding

--- src/main.rs
+++ src/main.rs
@@ -9,45 +9,70 @@ fn main() {
     let mut metadat_path = arguments.repository.clone();
     metadat_path.push("metadata");
 
-    let authors = metadat_path.read_dir()
+    let authors = metadat_path
+        .read_dir()
         .expect("Unable to read content of metadata")
-        .filter_map(|result| result.ok());
+        .filter_map(|result| {
+            if let Some(entry) = result.ok() {
+                if entry.file_type().is_ok_and(|file_type| file_type.is_dir()) {
+                    return Some(entry);
+                } else {
+                    return None;
+                }
+            } else {
+                return None;
+            }
+        });
 
     let mut index = IndexMap::new();
 
     for author in authors {
-        if !author.file_type().is_ok_and(|item| item.is_dir()) {
-            continue;
-        }
         index.insert(
-            author.path().file_stem()
+            author
+                .path()
+                .file_name()
                 .expect("Unable to get filename")
-                .to_os_string().into_string()
-                .expect("Unable to convert OsString to String"), 
-            author.path().read_dir().expect("Unable to read content of author")
-                .filter_map(|result| result.ok())
-                .filter_map(|item| {
-                    if item.file_type().is_ok_and(|item| item.is_file())
-                        && item.path().extension().map_or(false, |extention| extention == "yml") {
-                        return Some(
-                            item.path().file_stem()
-                                .expect("Unable to get filename")
-                                .to_os_string().into_string()
-                                .expect("Unable to convert OsString to String")
-                        );
+                .to_os_string()
+                .into_string()
+                .expect("Unable to convert OsString to String"),
+            author
+                .path()
+                .read_dir()
+                .expect("Unable to read content of author")
+                .filter_map(|result| {
+                    if let Some(entry) = result.ok() {
+                        if !entry.file_type().is_ok_and(|file_type| file_type.is_file()) {
+                            return None;
+                        }
+                        if !entry
+                            .path()
+                            .extension()
+                            .map_or(false, |extention| extention == "yml")
+                        {
+                            return None;
+                        }
+                        if let Some(stem) = entry.path().file_stem() {
+                            return Some(
+                                stem.to_os_string()
+                                    .into_string()
+                                    .expect("Unable to convert OsString to String"),
+                            );
+                        } else {
+                            return None;
+                        }
                     } else {
-                        return None
+                        return None;
                     }
                 })
-                .collect::<Vec<_>>()
+                .collect::<Vec<_>>(),
         );
     }
 
     if let Some(parent) = arguments.output_path.parent() {
         std::fs::create_dir_all(parent).expect("Unable to create parents");
     }
-    let output = std::fs::File::create(&arguments.output_path)
-        .expect("Unable to create output path");
+    let output =
+        std::fs::File::create(&arguments.output_path).expect("Unable to create output path");
     let writer = std::io::BufWriter::new(output);
     serde_json::to_writer(writer, &index).expect("Unable to serialize JSON");
 }
