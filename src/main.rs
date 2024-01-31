3e0a492030104cdd4d4f587c36b214cf0553f154
lucka-me <i@lucka.moe>
2024-01-31 17:07:03 +0800

    Improve the index structure - Convert to object arrays for future extending

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use clap::Parser;
 
-type FilenameVec = std::vec::Vec<std::string::String>;
-type IndexMap = std::collections::HashMap<std::string::String, FilenameVec>;
+#[derive(Debug, clap::Parser)]
+#[command(author, version, about)]
+struct Arguments {
+    #[arg(long, value_hint = clap::ValueHint::FilePath)]
+    repository: std::path::PathBuf,
+
+    #[arg(long, value_hint = clap::ValueHint::FilePath)]
+    output_path: std::path::PathBuf,
+}
+
+#[derive(Debug, serde::Serialize)]
+struct PluginMetadata {
+    filename: std::string::String,
+}
+
+#[derive(Debug, serde::Serialize)]
+struct Author {
+    name: std::string::String,
+    plugins: std::vec::Vec<PluginMetadata>,
+}
 
 fn main() {
     let arguments = Arguments::parse();
 
-    let mut metadat_path = arguments.repository.clone();
-    metadat_path.push("metadata");
+    let mut metadata_path = arguments.repository.clone();
+    metadata_path.push("metadata");
 
-    let authors = metadat_path
+    let index = metadata_path
         .read_dir()
         .expect("Unable to read content of metadata")
         .filter_map(|result| {
@@ -22,51 +40,52 @@ fn main() {
             } else {
                 return None;
             }
-        });
-
-    let mut index = IndexMap::new();
-
-    for author in authors {
-        index.insert(
-            author
-                .path()
-                .file_name()
-                .expect("Unable to get filename")
-                .to_os_string()
-                .into_string()
-                .expect("Unable to convert OsString to String"),
-            author
-                .path()
-                .read_dir()
-                .expect("Unable to read content of author")
-                .filter_map(|result| {
-                    if let Some(entry) = result.ok() {
-                        if !entry.file_type().is_ok_and(|file_type| file_type.is_file()) {
-                            return None;
-                        }
-                        if !entry
-                            .path()
-                            .extension()
-                            .map_or(false, |extention| extention == "yml")
-                        {
-                            return None;
-                        }
-                        if let Some(stem) = entry.path().file_stem() {
-                            return Some(
-                                stem.to_os_string()
-                                    .into_string()
-                                    .expect("Unable to convert OsString to String"),
-                            );
+        })
+        .map(|author_entry| {
+            let author_path = author_entry.path();
+            return Author {
+                name: author_path
+                    .file_name()
+                    .expect("Unable to get filename")
+                    .to_os_string()
+                    .into_string()
+                    .expect("Unable to convert OsString to String"),
+                plugins: author_path
+                    .read_dir()
+                    .expect("Unable to read content of author")
+                    .filter_map(|result| {
+                        if let Some(entry) = result.ok() {
+                            if !entry.file_type().is_ok_and(|file_type| file_type.is_file()) {
+                                return None;
+                            }
+                            if !entry
+                                .path()
+                                .extension()
+                                .map_or(false, |extention| extention == "yml")
+                            {
+                                return None;
+                            }
+                            return Some(entry);
                         } else {
                             return None;
                         }
-                    } else {
-                        return None;
-                    }
-                })
-                .collect::<Vec<_>>(),
-        );
-    }
+                    })
+                    .filter_map(|plugin_entry| {
+                        if let Some(stem) = plugin_entry.path().file_stem() {
+                            return Some(PluginMetadata {
+                                filename: stem
+                                    .to_os_string()
+                                    .into_string()
+                                    .expect("Unable to convert OsString to String"),
+                            });
+                        } else {
+                            return None;
+                        }
+                    })
+                    .collect::<Vec<_>>(),
+            };
+        })
+        .collect::<Vec<_>>();
 
     if let Some(parent) = arguments.output_path.parent() {
         std::fs::create_dir_all(parent).expect("Unable to create parents");
@@ -75,14 +94,4 @@ fn main() {
         std::fs::File::create(&arguments.output_path).expect("Unable to create output path");
     let writer = std::io::BufWriter::new(output);
     serde_json::to_writer(writer, &index).expect("Unable to serialize JSON");
-}
-
-#[derive(Debug, Parser)]
-#[command(author, version, about)]
-struct Arguments {
-    #[arg(long, value_hint = clap::ValueHint::FilePath)]
-    repository: std::path::PathBuf,
-
-    #[arg(long, value_hint = clap::ValueHint::FilePath)]
-    output_path: std::path::PathBuf,
 }
